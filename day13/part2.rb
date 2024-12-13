ae040f193e3cf1f513a8f7275cdf1753ae28004f
Johannes Opper <xijo@pm.me>
2024-12-13 14:34:49 +0100

    day13

---
+++ day13/part2.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# https://adventofcode.com/2024/day/13
+
+input = File.read('day13/input').strip
+# input = File.read('day13/example').strip
+
+machines = input.split("\n\n").map do |machine|
+  machine.scan(/\d+/).map(&:to_i)
+end
+
+# use cramers theorem to solve the system of equations
+solutions = machines.map do |ax, ay, bx, by, px, py|
+  px += 10000000000000
+  py += 10000000000000
+  press_a = (px * by - py * bx).to_f / (ax * by - ay * bx)
+  press_b = (ax * py - ay * px).to_f / (ax * by - ay * bx)
+
+  if press_a == press_a.round && press_b == press_b.round
+    (press_a * 3 + press_b).round
+  end
+end.compact
+
+puts solutions.sum
